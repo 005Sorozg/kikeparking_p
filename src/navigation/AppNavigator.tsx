78a36f5fab3b51573dbb599a4ed032962e51ca14
Samuel <samiorozcog@gmail.com>
2026-09-19 16:43:37 -0500

    feat: agregar pantalla de registro

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import type { AppStackParamList } from "../types/navigation";
 import { LoginScreen } from "../screens/LoginScreen";
+import { RegisterScreen } from "../screens/RegisterScreen";
 
 const Stack = createNativeStackNavigator<AppStackParamList>();
 
@@ -15,6 +16,11 @@ export function AppNavigator() {
           component={LoginScreen}
           options={{ headerShown: false }}
         />
+         <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ title: "Crear cuenta" }}
+        />      
       </Stack.Navigator>
     </NavigationContainer>
   );
